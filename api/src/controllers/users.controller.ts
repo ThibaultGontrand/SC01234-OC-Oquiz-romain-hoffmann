a9289b313d56cff5c312660651d6c6a0719e26c1
Romain H <hoffmann.digital.formation@gmail.com>
2026-03-28 03:28:46 +0100

    feat: BDD models connexion

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -1,18 +1,10 @@
 import type { Request, Response } from "express";
+import { prisma } from "../models/index.ts";
 
 export async function getAllUsers(req: Request, res: Response) {
-    // Exemple
-    const users = [
-        {
-            id: 1,
-            nom: "toto"
-        },
-        {
-            id: 2,
-            nom: "tata"
-        }
-    ];
-
+    // Appel la BDD
+    const users = await prisma.user.findMany();
+  
     // Réponse au client
     res.json(users);
-}
+  }
