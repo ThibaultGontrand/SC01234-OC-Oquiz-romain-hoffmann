7cb38585adde2f60e0c901a3cacf2d2095f78ac6
Romain H <hoffmann.digital.formation@gmail.com>
2026-03-29 16:52:36 +0200

    feat: api: ajout cors

--- api/config.ts
+++ api/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   port: parseInt(process.env.PORT || "3000"),
   databaseUrl: process.env.DATABASE_URL || "",
+  allowedOrigins: process.env.ALLOWED_ORIGINS || "*",
 };
--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,13 @@
+import cors from "cors";
 import express from "express";
 import { router as apiRouter } from "./routers/index.router.ts";
+import { config } from "../config.ts";
 
 // Créer une app Express
 export const app = express();
+
+// Autoriser les requêtes cross-origin
+app.use(cors({ origin: config.allowedOrigins }));
 
 // Body parser pour récupérer les body "application/json" dans req.body
 app.use(express.json());
